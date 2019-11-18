cb2b55f3f7258ae10bc8c9d93cdc896cd166d9bf
chaitanya <chaitanyasraut@gmail.com>
2019-11-19 01:16:04 +0530

    first commit for writing parser script

--- EFS.py
+++ EFS.py
@@ -5,19 +5,22 @@ Created on Tue Mar 20 16:02:27 2018
 @author: Chaitanya Raut     
 """
 
-import urllib2
+import requests
 
-response = urllib2.urlopen('http://www.espnfc.com/spanish-primera-division/15/statistics/scorers')
+response = requests.get('https://www.fotmob.com/leagues/87/stats')
 
-page = response.read()  #Reads the url
-page.strip()       #Removes white spaces
+#print(response.text)
+
+response.json()
+
+"""
 index = page.find('"rank">1') #Start of the table at the webpage
 index1 = page.find('"rank">4',index) #End of the data which we are interested
 player_data = page[index:index1-23]
 player_data = ' '.join(player_data.split())
 
-print player_data
- 
+
+print(player_data)
 URL2 = urllib2.urlopen('http://www.espnfc.us/english-premier-league/23/statistics/scorers')
 page1 = URL2.read()  #Reads the url
 page1.strip()       #Removes white spaces
@@ -27,3 +30,4 @@ player_data1 = page1[index:index1-23]
 player_data1 = ' '.join(player_data1.split())
 
 print player_data1
+"""
